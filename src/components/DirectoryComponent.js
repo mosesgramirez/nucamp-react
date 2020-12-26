c5dc836ba37928562df969875b059353393b4bf9
Moses Ramirez <mosesgramirez@gmail.com>
2020-12-26 11:43:57 -0600

    Components pt2 update

---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Directory extends Component {
+    // constructor is optional, but if you have one, the props param is necessary, along with the super(props) line.
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCampsite: null
+        };
+    }
+
+    onCampsiteSelect(campsite) {
+        this.setState({selectedCampsite: campsite});
+    }
+
+    renderSelectedCampsite(campsite) {
+        if(campsite) {
+            return (
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        };
+        return <div />;
+    }
+
+    render() {
+        const directory = this.props.campsites.map(campsite => {
+            return (
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
+                <div className="row">
+                    <div className="col-md-5 m-1"> 
+                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+/*
+class ExampleParentComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 333
+        }
+    }
+    render() {
+        return <ExampleChildComponent number={this.state.number} greeting="Hello World"/>
+    }
+}
+
+class ExampleChildComponent extends Component {
+    render () {
+        return <div>{this.props.number} {this.props.greeting}</div>
+    }
+}
+*/
+
+
+
+export default Directory;
